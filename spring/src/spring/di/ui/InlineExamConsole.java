d05e32b6357bd9be99080d288dbb64a8ff1653d1
woosupyun <yunwoosup@naver.com>
2020-09-13 21:26:02 +0900

    Autowired의 위치는 3가지 정도 있다. 1. 변수 위에 위치 ( 기본생성자를 통한 Injection ) 2. setter 위에 위치 ( setter를 통한 Injection ) 3. overload 생성자 위에 위치 ( overload 생성자를 통한 Injection)
    required속성 사용하기
    객체가 없어도 null인 상태로 Injection하고 싶을 경우 required = false을 사용한다.

--- spring/src/spring/di/ui/InlineExamConsole.java
+++ spring/src/spring/di/ui/InlineExamConsole.java
@@ -6,25 +6,31 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import spring.di.entity.Exam;
 
 public class InlineExamConsole implements ExamConsole {
-
+	
+	@Autowired(required = false)
+	@Qualifier("exam2")
 	private Exam exam;
 	
 	public InlineExamConsole() {
+		System.out.println("constructor");
 	}
 	
 	public InlineExamConsole(Exam exam) {
+		System.out.println("overloaded constructor");
 		this.exam = exam;
 	}
 
 	@Override
 	public void print() {
-		System.out.printf("total is %d, avg is %f \n", exam.total(), exam.avg());
+		if(exam == null)
+			System.out.printf("total is %d, avg is %f \n", 0, 0.0);
+		else
+			System.out.printf("total is %d, avg is %f \n", exam.total(), exam.avg());
 	}
-	
-	@Autowired
-	@Qualifier("exam2")
+
 	@Override
 	public void setExam(Exam exam) {
+		System.out.println("setter constructor");
 		this.exam = exam;
 	}
 
