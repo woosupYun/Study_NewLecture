541ec3ab02fe4a480fe2ac5361a31e6467ae9581
woosupyun <yunwoosup@naver.com>
2020-09-14 03:05:09 +0900

    spring을 이용해 AOP 구현하기

--- spring/src/spring/aop/Program.java
+++ spring/src/spring/aop/Program.java
@@ -4,32 +4,42 @@ import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 import spring.aop.entity.Exam;
 import spring.aop.entity.NewLecExam;
 
 public class Program {
 
 	public static void main(String[] args) {
-		Exam exam = new NewLecExam(1,1,1,1);
 		
-		Exam proxy = (Exam) Proxy.newProxyInstance(NewLecExam.class.getClassLoader(), new Class[] {Exam.class}, new InvocationHandler() {
-			
-			@Override
-			public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-				long start = System.currentTimeMillis();
-				
-				Object result = method.invoke(exam, args);
-				
-				long end = System.currentTimeMillis();
-				String message = (end - start) + " ms 시간이 걸렸습니다.";
-				System.out.println(message);
-				
-				return result;
-			}
-		});
+		ApplicationContext context =
+				//new AnnotationConfigApplicationContext(NewlecDIConfig.class);
+				new ClassPathXmlApplicationContext("spring/aop/setting.xml");	
+
+		Exam exam = (Exam) context.getBean("exam");
 		
-		System.out.printf("total is %d \n", proxy.total());
-		System.out.printf("avg is %f \n", proxy.avg());
+		System.out.printf("total is %d \n", exam.total());
+		System.out.printf("avg is %f \n", exam.avg());
+		
+//		Exam exam = new NewLecExam(1,1,1,1);
+//		
+//		Exam proxy = (Exam) Proxy.newProxyInstance(NewLecExam.class.getClassLoader(), new Class[] {Exam.class}, new InvocationHandler() {
+//			
+//			@Override
+//			public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+//				long start = System.currentTimeMillis();
+//				
+//				Object result = method.invoke(exam, args);
+//				
+//				long end = System.currentTimeMillis();
+//				String message = (end - start) + " ms 시간이 걸렸습니다.";
+//				System.out.println(message);
+//				
+//				return result;
+//			}
+//		});
 
 	}
 
